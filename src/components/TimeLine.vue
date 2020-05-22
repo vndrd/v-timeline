0eb4bd1839e9f0d7829a4a2d55d114f69aec5d1f
vndrd <ivangddzzz@gmail.com>
2020-05-22 11:58:44 -0400

    Archivos separados

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,118 @@
+<template>
+<div class="timeline-wrapper">
+    <li v-for="(item,index) in lista" :key="item.id" class="timeline-item">
+        <div class="fecha float-left">
+            <span style="display:none">{{index}}</span>
+            {{item.fecha}}
+        </div>
+        <div class="suceso">
+            <div class="card">
+            <div class="card-header">
+                {{item.title}}
+            </div>
+            <div class="card-body">
+                {{item.content}}
+            </div>
+            </div>
+            <br>
+        </div>
+    </li>
+</div>    
+</template>
+<script>
+export default {
+    name: 'TimeLine',
+    props: ['lista'],
+}
+</script>
+<style lang="scss" scoped>
+.float-right{
+  float: right;
+  text-align: left;
+}
+.float-left{
+  float: left;
+  text-align: right;
+  padding-left: 100px;
+  background: #aaa;
+}
+.timeline-item{  
+  display: flex;
+  position: relative;
+  // alinear
+  justify-content: center;
+}
+.suceso{
+  // background: #449;
+  margin-bottom: 20px;
+  padding: 10px;
+  width: 20%;
+}
+.fecha{
+  color: #aaa;
+  font-weight: 800;
+  background: white;
+  padding: 10px;
+  width: 20%;
+  position: relative;
+  
+}
+.card{
+  margin-left: 20px;
+  background: white;
+  min-height: 100px;
+}
+.card-header::after{
+  content:'';
+  z-index: 9999;
+  position: absolute;
+  left: 0;
+  top:0;
+  border-left: 25px solid transparent;
+  border-bottom: 25px solid transparent;
+  border-top: 25px solid #844;
+  transform: translateX(-20px);
+}
+.card-header {
+  position:relative;
+  color: #fff;
+  padding:10px;
+  background: #844;
+}
+
+.card-body {
+  background: #ddd;
+  min-height: 80px;
+}
+
+/*timeline*/
+
+.fecha::after{
+  position:absolute;
+  right: 0;
+  transform: translateX(4px);
+  width: 10px;
+  height: 10px;
+  background: #844;
+  border: #a66 1px solid;
+  border-radius: 10px;
+  content:'';
+}
+.fecha::before{
+  position:absolute;
+  z-index: 9999;
+  right: 0;
+  width: 4px;
+  transform: translateY(15px);
+  height: calc(90%);
+  background: #8446;
+  content:'';
+}
+.fecha::before:nth-last-child(){
+  position:absolute;
+  right: -6px;
+  width: 2px;
+  background: black;
+  content:'';
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,115 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <div class="timeline-wrapper">
-        <li v-for="(item,index) in lista" :key="item.id" class="timeline-item">
-            <div class="fecha float-left">
-              <span style="display:none">{{index}}</span>
-              {{item.fecha}}
-            </div>
-            <div class="suceso">
-              <div class="card">
-                <div class="card-header">
-                  {{item.title}}
-                </div>
-                <div class="card-body">
-                  {{item.content}}
-                </div>
-              </div>
-              <br>
-            </div>
-        </li>
+    <div class="img-cont">
+      <!-- <img alt="Vue logo" src="../assets/timeline.svg"> -->
+      <h1>Timeline</h1>
     </div>
+    <TimeLine :lista="lista"/>
   </div>
 </template>
 <style lang="scss" scoped>
-.float-right{
-  float: right;
-  text-align: left;
+.img-cont{
+  width: 200px;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
 }
-.float-left{
-  float: left;
-  text-align: right;
-  padding-left: 100px;
-  background: #aaa;
-}
-.timeline-item{
-  
-  display: flex;
-  position: relative;
-  // alinear
-  justify-content: center;
-}
-.suceso{
-  // background: #449;
-  margin-bottom: 20px;
-  padding: 10px;
-  width: 20%;
-}
-.fecha{
-  color: #aaa;
-  font-weight: 800;
-  background: white;
-  padding: 10px;
-  width: 20%;
-  position: relative;
-  
-}
-.card{
-  margin-left: 20px;
-  background: white;
-  min-height: 100px;
-}
-.card-header::after{
-  content:'';
-  z-index: 9999;
-  position: absolute;
-  left: 0;
-  top:0;
-  border-left: 25px solid transparent;
-  border-bottom: 25px solid transparent;
-  border-top: 25px solid #844;
-  transform: translateX(-20px);
-}
-.card-header {
-  position:relative;
-  color: #fff;
-  padding:10px;
-  background: #844;
-}
-
-.card-body {
-  background: #ddd;
-  min-height: 80px;
-}
-
-
-
-.fecha::after{
-  position:absolute;
-  right: -10px;
-  width: 10px;
-  height: 10px;
-  background: #844;
-  border: #a66 1px solid;
-  border-radius: 10px;
-  content:'';
-}
-.fecha::before{
-  position:absolute;
-  z-index: 9999;
-  right: -5px;
-  width: 4px;
-  height: calc(100%);
-  background: #8446;
-  content:'';
-}
-.fecha::before:nth-last-child(){
-  position:absolute;
-  right: -6px;
-  width: 2px;
-  background: black;
-  content:'';
+img{
+  width: 100%;
+  transform: rotate(135deg);
 }
 
 </style>
@@ -117,19 +24,19 @@
 <script>
 // @ is an alias to /src
 
-
+import TimeLine from '@/components/TimeLine'
 export default {
   name: 'Home',
   components: {
-    
+    TimeLine
   },
   data(){
     return {
       lista: [
-        {id : 1 , title: 'Titulo 1', content: 'contenido 1' , fecha:'01-05-19'},
-        {id : 2 , title: 'Titulo 2', content: 'contenido 2' , fecha:'02-05-19'},
-        {id : 3 , title: 'Titulo 3', content: 'contenido 3' , fecha:'03-05-19'},
-        {id : 4 , title: 'Titulo 4', content: 'contenido 4' , fecha:'04-05-19'},
+        {id : 1 , title: 'Titulo 1', content: 'contenido 1' , fecha:'01.05.20'},
+        {id : 2 , title: 'Titulo 2', content: 'contenido 2' , fecha:'02.05.20'},
+        {id : 3 , title: 'Titulo 3', content: 'contenido 3' , fecha:'03.05.20'},
+        {id : 4 , title: 'Titulo 4', content: 'contenido 4' , fecha:'04.05.20'},
       ]
     }
   }
