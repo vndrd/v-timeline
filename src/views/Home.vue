e863174066d11e630d055a10f611ef465f432847
vndrd <ivangddzzz@gmail.com>
2020-05-21 23:55:08 -0400

    estructura base

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="timeline-wrapper">
+        <li v-for="(item,index) in lista" :key="item.id" class="timeline-item">
+            <div class="fecha float-left">
+              {{index}}
+            </div>
+            <div class="suceso float-right">
+              {{item.title}}
+              {{item.content}}
+              <br>
+              <div v-if="index==2">
+                {{item.title}}
+                {{item.content}}
+              </div>
+            </div>
+        </li>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.float-right{
+  float: right;
+}
+.float-left{
+  float: left;
+}
+.timeline-item{
+  background: aqua;
+  display: flex;
+  // alinear
+  justify-content: center;
+}
+.fecha{
+  background: white;
+  padding: 10px;
+  width: 20%;
+  position: relative;
+  
+}
+.fecha::after{
+  position:absolute;
+  right: -10px;
+  width: 10px;
+  height: 10px;
+  background: black;
+  border-radius: 10px;
+  content:'';
+}
+.fecha::before{
+  position:absolute;
+  right: -6px;
+  width: 2px;
+  height: calc(100%);
+  background: black;
+  content:'';
+}
+.fecha::before:nth-last-child(){
+  position:absolute;
+  right: -6px;
+  width: 2px;
+  background: black;
+  content:'';
+}
+.suceso{
+  background: white;
+  padding: 10px;
+  width: 20%;
+}
+</style>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return {
+      lista: [
+        {id : 1 , title: 'Titulo 1', content: 'contenido 1'},
+        {id : 2 , title: 'Titulo 2', content: 'contenido 2'},
+        {id : 3 , title: 'Titulo 3', content: 'contenido 3'},
+        {id : 4 , title: 'Titulo 4', content: 'contenido 4'},
+      ]
+    }
   }
 }
 </script>
